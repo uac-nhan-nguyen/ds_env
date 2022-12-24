4967bde67a4e780ca89aa8a993be18bc82d3d00e
nhan.nguyen <nhan.nguyen@uac.edu.au>
2022-12-24 21:12:12 +0700

    Update dart

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 1.0.2
+
+- update to Dart 2.12 for null safety
+- 
+## 1.0.1
+
+- update README
+
 ## 1.0.0
 
 - add `Env`
--- lib/Env.dart
+++ lib/Env.dart
@@ -1,17 +1,16 @@
 import 'dart:io';
 
 class Env {
-  Map<String, String> _env;
+  final Map<String, String> _env;
 
-  Env(String filepath) {
-    _env = Map.fromEntries(File(filepath).readAsStringSync().split('\n').where((i) => i.isNotEmpty && i.contains('=')).map((e) {
-      final s = e.split('=');
-      return MapEntry(s[0], s[1]);
-    }));
-  }
+  Env(String filepath)
+      : _env = Map.fromEntries(File(filepath).readAsStringSync().split('\n').where((i) => i.isNotEmpty && i.contains('=')).map((e) {
+          final s = e.split('=');
+          return MapEntry(s[0], s[1]);
+        }));
 
   String operator [](String key) {
     if (_env[key] == null) throw 'Env Key[${key}] not found';
-    return _env[key];
+    return _env[key]!;
   }
 }
